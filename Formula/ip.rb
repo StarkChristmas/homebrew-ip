5fa92b3e065bf9dabdf35ae27382855a6d268bfc
Stark Christmas <stark@Starks-iMac.local>
2025-06-14 04:01:06 +0800

    fix desc format and restrict build to macOS only

--- Formula/ip.rb
+++ Formula/ip.rb
@@ -4,10 +4,10 @@ class Ip < Formula
   version "1.0.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/StarkChristmas/ipget/releases/download/v1.0.0/ip_macos_1.0.0_arm64.tar.gz"
+    url "https://github.com/StarkChristmas/ipget/releases/download/v1.0.0/ip_macos_v1.0.0_arm64.tar.gz"
     sha256 "c6093016dec0cb27576dcaa486fbe47140640646198ac57757392280635122c6"
   else
-    url "https://github.com/StarkChristmas/ipget/releases/download/v1.0.0/ip_macos_1.0.0_x86_64.tar.gz"
+    url "https://github.com/StarkChristmas/ipget/releases/download/v1.0.0/ip_macos_v1.0.0_x86_64.tar.gz"
     sha256 "04062e4b634778f68bad52ee8e5e02f1c13c28f210bce0252023e28f07174730"
   end
 
